a924f00cba1ee2cff92ded8e2d228983bb1226ca
jh-488 <jihadhyadi01@gmail.com>
2026-05-12 21:28:33 +0200

    update requirements

--- config.js
+++ config.js
@@ -1,1 +1,8 @@
 const API_BASE_URL = "http://localhost:8000";
+
+
+// Local development
+// const API_BASE_URL = "http://localhost:8000";
+
+// Production deployment
+// const API_BASE_URL = "https://oer-navigator-backend.onrender.com";
