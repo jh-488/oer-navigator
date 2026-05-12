560b1aeff09f880f79c871c33a157ee664b61cd9
Sebastian Zug <sebastian.zug@informatik.tu-freiberg.de>
2026-05-12 17:13:44 +0200

    Add openAI Interface

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/jihad-h/LiaScript/oer-navigator/.venv
+set -gx VIRTUAL_ENV /home/sz/Desktop/Python/oer-navigator/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
