615839b9e588af4221acb2382cdde18583ea50e0
jh-488 <jihadhyadi01@gmail.com>
2026-05-13 11:12:15 +0200

    update html title + remove docs

--- docs/config.js
+++
@@ -1,8 +0,0 @@
-const API_BASE_URL = "https://oer-navigator-server.onrender.com";
-
-
-// Local development
-// const API_BASE_URL = "http://localhost:8000";
-
-// Production deployment
-// const API_BASE_URL = "https://oer-navigator-server.onrender.com";
